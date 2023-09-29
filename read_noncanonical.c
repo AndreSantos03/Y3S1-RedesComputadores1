fcdcd9957d207d1005991ff81ee7bff129fa8647
Tomás Silva <113911294+tomasilva03@users.noreply.github.com>
2023-09-29 11:36:00 +0100

    Add files via upload

---
+++ read_noncanonical.c
@@ -0,0 +1,148 @@
+// Read from serial port in non-canonical mode
+//
+// Modified by: Eduardo Nuno Almeida [enalmeida@fe.up.pt]
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <termios.h>
+#include <unistd.h>
+
+// Baudrate settings are defined in <asm/termbits.h>, which is
+// included by <termios.h>
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 // POSIX compliant source
+
+#define FALSE 0
+#define TRUE 1
+
+#define BUF_SIZE 256
+#define flag 0x7E
+#define addr_s 0x03
+#define addr_r 0x01
+#define c_set 0x03
+#define c_ua 0x07
+
+
+volatile int STOP = FALSE;
+
+int main(int argc, char *argv[])
+{
+    // Program usage: Uses either COM1 or COM2
+    const char *serialPortName = argv[1];
+
+    if (argc < 2)
+    {
+        printf("Incorrect program usage\n"
+               "Usage: %s <SerialPort>\n"
+               "Example: %s /dev/ttyS1\n",
+               argv[0],
+               argv[0]);
+        exit(1);
+    }
+
+    // Open serial port device for reading and writing and not as controlling tty
+    // because we don't want to get killed if linenoise sends CTRL-C.
+    int fd = open(serialPortName, O_RDWR | O_NOCTTY);
+    if (fd < 0)
+    {
+        perror(serialPortName);
+        exit(-1);
+    }
+
+    struct termios oldtio;
+    struct termios newtio;
+
+    // Save current port settings
+    if (tcgetattr(fd, &oldtio) == -1)
+    {
+        perror("tcgetattr");
+        exit(-1);
+    }
+
+    // Clear struct for new port settings
+    memset(&newtio, 0, sizeof(newtio));
+
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    // Set input mode (non-canonical, no echo,...)
+    newtio.c_lflag = 0;
+    newtio.c_cc[VTIME] = 0; // Inter-character timer unused
+    newtio.c_cc[VMIN] = 5;  // Blocking read until 5 chars received
+
+    // VTIME e VMIN should be changed in order to protect with a
+    // timeout the reception of the following character(s)
+
+    // Now clean the line and activate the settings for the port
+    // tcflush() discards data written to the object referred to
+    // by fd but not transmitted, or data received but not read,
+    // depending on the value of queue_selector:
+    //   TCIFLUSH - flushes data received but not read.
+    tcflush(fd, TCIOFLUSH);
+
+    // Set new port settings
+    if (tcsetattr(fd, TCSANOW, &newtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    printf("New termios structure set\n");
+
+    // Loop for input
+    unsigned char buf[BUF_SIZE + 1] = {0}; // +1: Save space for the final '\0' char
+    
+    unsigned char bytes_received[BUF_SIZE + 1] = {0};
+    
+
+    bytes_received[0] = flag;
+    bytes_received[1] = addr_s;
+    bytes_received[2] = c_set;
+    bytes_received[3] = buf[1]^buf[2];
+    bytes_received[4] = flag;
+
+    unsigned char bytes_sent[BUF_SIZE + 1] = {0};
+    
+
+    bytes_sent[0] = flag;
+    bytes_sent[1] = addr_s;
+    bytes_sent[2] = c_ua;
+    bytes_sent[3] = buf[1]^buf[2];
+    bytes_sent[4] = flag;
+
+    int bytes = read(fd, buf, BUF_SIZE);
+    if(buf[1] == bytes_received[1] && buf[2] == bytes_received[2]){
+        printf("received ua");
+        write(fd, bytes_sent, BUF_SIZE);
+    } 
+    
+    /*while (STOP == FALSE)
+    {
+        // Returns after 5 chars have been input
+        int bytes = read(fd, buf, BUF_SIZE);
+        buf[bytes] = '\0'; // Set end of string to '\0', so we can printf
+
+        printf(":%s:%d\n", buf, bytes);
+        if (buf[0] == 'z')
+            STOP = TRUE;
+    }*/
+
+    // The while() cycle should be changed in order to respect the specifications
+    // of the protocol indicated in the Lab guide
+
+    // Restore the old port settings
+    if (tcsetattr(fd, TCSANOW, &oldtio) == -1)
+    {
+        perror("tcsetattr");
+        exit(-1);
+    }
+
+    close(fd);
+
+    return 0;
+}
