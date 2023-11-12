a438dc62b920232ca29ef2134451fe86a2fcf32a
AndreSantos03 <andrebernardosantos@gmail.com>
2023-11-12 19:03:46 +0000

    Please man cmon

--- include/link_layer.h
+++ include/link_layer.h
@@ -65,16 +65,11 @@ typedef enum {
 #define C_REJ(Nr) ((Nr << 7) | 0x01)
 #define C_N(Ns) (Ns << 6)
  */
+#define C_RR(tramaRx) ((tramaRx == 0) ? 0x05 : 0x85)
+#define C_REJ(tramaRx) ((tramaRx == 0) ? 0x01 : 0x81)
+#define C_N(Ns) ((Ns == 0) ? 0x00 : 0x40)
 
 
-// Macro to generate control field for C_RR based on tramaRx (0 or 1)
-#define CONTROL_FIELD_RR(tramaRx) ((tramaRx == 0) ? 0x05 : 0x85)
-
-// Macro to generate control field for C_REJ based on tramaRx (0 or 1)
-#define CONTROL_FIELD_REJ(tramaRx) ((tramaRx == 0) ? 0x01 : 0x81)
-
-// Macro to generate control field for C_N based on Ns (0 or 1)
-#define CONTROL_FIELD_N(Ns) ((Ns == 0) ? 0x00 : 0x40)
 
 
 // Function to establish a connection using the specified parameters.
